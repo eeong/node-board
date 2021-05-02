78e523832b40147c1f03ddfd822ad11c29df58d0
eeong <discography8@gmail.com>
2021-05-02 17:35:08 +0900

    210502 season2 rank update

--- api/controllers/searchController.js
+++ api/controllers/searchController.js
@@ -3,13 +3,14 @@ const qs = require('querystring');
 const { weapon, armor, charList, getItem } = require('../modules/getJson');
 const refreshJson = {"code":429,"type":'refresh'};
 const fetchHeaders = {'accept': 'application/json','x-api-key': `${process.env.X_API_KEY}`}
+const seasonId = 3;
 
 const mongoose = require('mongoose');
 const itemA = mongoose.model('itemArmor')
 const itemW = mongoose.model('itemWeapon')
 
 exports.read_rank = async (req, res) => {
-  await fetch(`https://open-api.bser.io/v1/rank/top/1/`+req.query.m, {
+  await fetch(`https://open-api.bser.io/v1/rank/top/${seasonId}/`+req.query.m, {
     headers: fetchHeaders
 }).then( async ( response ) => {
   if(response.ok){
@@ -35,7 +36,6 @@ exports.read_user_num = async (req, res) => {
     for(var i in data.userGames){
       data.userGames[i].item = [];
       let charNum = data.userGames[i].characterNum-1
-      //console.log(data.userGames[i].characterNum);
       data.userGames[i].characterSrc = charList[charNum].slice(1,-1);
       data.userGames[i].nameKr = (charList[charNum].split('-'))[1].split('.')[0];
       for(var j in data.userGames[i].equipment){
@@ -59,7 +59,7 @@ exports.read_user_rank = (req, res) => {
   headers: fetchHeaders
 }).then( (response) => {
   response.json().then((data)=>{
-  fetch(`https://open-api.bser.io/v1/rank/${data.user.userNum}/1/${gameMode}`, {
+  fetch(`https://open-api.bser.io/v1/rank/${data.user.userNum}/${seasonId}/${gameMode}`, {
   headers: fetchHeaders
 }).then( async ( response ) => {
   await response.json().then((data)=>{
@@ -74,7 +74,7 @@ exports.read_user_rank = (req, res) => {
 
 
 exports.read_mmr = (req, res) => {
-  fetch(`https://open-api.bser.io/v1/rank/${req.params.usernum}/1/${req.params.mode}`, {
+  fetch(`https://open-api.bser.io/v1/rank/${req.params.usernum}/${seasonId}/${req.params.mode}`, {
   headers: fetchHeaders
 }).then( async ( response ) => {
   await response.json().then((data)=>{
